20933cb82e9bd395fe79e1fa56c0d2c9b54cb6f7
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2019-10-23 22:07:25 +0530

    Update shm.c

--- shm.c
+++ shm.c
@@ -7,13 +7,12 @@ using namespace std;
   
 int main() 
 { 
-    // ftok to generate unique key 
+  
     key_t key = ftok("shmfile",65); 
   
-    // shmget returns an identifier in shmid 
     int shmid = shmget(key,1024,0666|IPC_CREAT); 
   
-    // shmat to attach to shared memory 
+
     char *str = (char*) shmat(shmid,(void*)0,0); 
   
     cout<<"Write Data : "; 
@@ -21,7 +20,7 @@ int main()
   
     printf("Data written in memory: %s\n",str); 
       
-    //detach from shared memory  
+ 
     shmdt(str); 
   
     return 0; 
